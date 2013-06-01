19627241c1d6868cc6502b01b28b5a290781fde0
Marcin Wichary <mwichary@gmail.com>
2013-06-01 09:46:14 -0700

    Return arbitrary data structure at GET /v1/users/{id}

--- restapi/resources/v1/users.js
+++ restapi/resources/v1/users.js
@@ -118,6 +118,7 @@ exports.get = function(req, res) {
         }
         
         userJson.profileImageUrl = data.profile_image_url
+        userJson.data = user.data
         res.send(200, userJson)
 
       })
