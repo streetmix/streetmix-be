dc9b000b74bef742733fc688681071fb86969872
ycombinator <ycombinator@gmail.com>
2013-06-26 09:23:19 -0700

    Revert "Better handling of OPTIONS pre-flight requests?"
    This reverts commit 2b4f9dee40debe1f15d68b0f17d0941244d12251.

--- restapi/server.js
+++ restapi/server.js
@@ -30,24 +30,22 @@ var loginTokenParser = function(req, res, next) {
   next()
 } // END function - loginTokenParser
 
-var allowCrossDomain = function(req, res, next) {
+var unknownMethodHandler = function(req, res) {
+  if (req.method.toLowerCase() === 'options') {
+    var allowHeaders = ['Accept', 'Accept-Encoding', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'Authorization', 'Cache-Control' ]
 
-  var allowHeaders = ['Accept', 'Accept-Encoding', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'Authorization', 'Cache-Control' ]
-  
-  if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS')
-  
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Headers', allowHeaders.join(', '))
-  res.header('Access-Control-Allow-Methods', res.methods.join(', '))
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
- 
-  if (req.method.toLowerCase() === 'options') {   
+    if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS')
+    
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Headers', allowHeaders.join(', '))
+    res.header('Access-Control-Allow-Methods', res.methods.join(', '))
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+    
     return res.send(204)
-  } else {
-    return next()
   }
-
-} // END function - allowCrossDomain
+  else
+    return res.send(new restify.MethodNotAllowedError())
+}
 
 var customCacheControlHeaders = function(req, res, next) {
   res.header('Pragma', 'no-cache')
@@ -57,12 +55,11 @@ var customCacheControlHeaders = function(req, res, next) {
 } // END function - customCacheControlHeaders
 
 server.on('MethodNotAllowed', requestLog)
-server.on('MethodNotAllowed', allowCrossDomain)
+server.on('MethodNotAllowed', unknownMethodHandler)
 
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.CORS())
-server.use(allowCrossDomain)
 server.use(restify.fullResponse())
 server.use(loginTokenParser)
 server.use(requestLog)
