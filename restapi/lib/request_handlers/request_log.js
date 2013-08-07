8e4662bf07f334a7013f1e52c4c09cd6894cf6ad
ycombinator <ycombinator@gmail.com>
2013-08-07 06:35:26 -0700

    Not logging request body at debug level to reduce noise.

--- restapi/lib/request_handlers/request_log.js
+++ restapi/lib/request_handlers/request_log.js
@@ -9,6 +9,6 @@ module.exports = function(req, res, next) {
   var body = req.body || ''
   var now = new Date()
   var date = dateformat(now, "m/d/yyyy H:MM:ss Z")
-  req.log.debug({ method: req.method, url: req.url, content_type: contentType, body: body, login_token: loginToken})
+  req.log.debug({ method: req.method, url: req.url, content_type: contentType, login_token: loginToken})
   next()
 }
